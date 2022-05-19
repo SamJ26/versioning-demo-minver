2d8df79fbaea978b37b38fa3bfc53f916ad41da8
Samuel Janek <samuel.janek@chyron.com>
2022-05-19 15:41:18 +0200

    Add basic project structure

---
+++ MinVerDemo.Project1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MinVerDemo.Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
